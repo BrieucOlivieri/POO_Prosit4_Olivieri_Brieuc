b563918fe4bee9679e8b070a7e4fe0b1562f7683
Brieuc OLIVIERI <brieuc.olivieri@viacesi.fr>
2023-11-22 13:59:01 +0100

    New version with choice of station

--- POO_Prosit4_Olivieri_Brieuc/POO_Prosit4_Olivieri_Brieuc.cpp
+++ POO_Prosit4_Olivieri_Brieuc/POO_Prosit4_Olivieri_Brieuc.cpp
@@ -4,8 +4,53 @@
 
 using namespace std;
 
+void Nrj(void);
+void Sky(void);
+void FrTnter(void);
+
+typedef void (*pf)(void);
+pf* index;
+
+void Nrj(void)
+{
+	EM NRJ;
+	NRJ.signal("Vous ecoutez NRJ !");
+}
+void Sky(void)
+{
+	EM Skyrock;
+	Skyrock.signal("Vous ecoutez SKYROCK !");
+}
+void FrTnter(void)
+{
+	EM FranceInter;
+	FranceInter.signal("Vous ecoutez FranceInter !");
+}
+
 int main()
 {
-	EM emission;
-	emission.signal("Salut");
+	int nombre;
+	pf addOfFunc[3];
+
+	addOfFunc[0] = &Nrj; addOfFunc[1] = &Sky; addOfFunc[2] = &FrTnter;
+
+	cout << "Combien de radios voulez-vous ?" << endl;
+	cin >> nombre;
+
+	index = new pf[nombre];
+
+	for (int i = 0; i < nombre; i++)
+	{
+		int ii;
+
+		cout << "Quelle radios souhaitez-vous a l'indice " << i << " de votre index de radios ?" << endl << "NRJ = 0; Skyrock = 1; FranceTnter = 2" << endl;
+		cin >> ii;
+
+		index[i] = addOfFunc[ii];
+	}
+
+	for (int i = 0; i < nombre; i++)
+	{
+		index[i]();
+	}
 }
